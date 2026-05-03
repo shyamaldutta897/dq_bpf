e1e4aa408ec59554379d7afea1d22b00fa02fc4e
Shyamal Dutta <shyamaldutta897@gmail.com>
2026-05-03 11:21:59 +0530

    updated functions

--- dq_checks/double_check.py
+++ dq_checks/double_check.py
@@ -3,7 +3,8 @@ from pyspark.sql.types import *
 
 def double_check(df,column):
     total=df.count()
-    failed=df.filter(typeof(column).cast(StringType())!="double").count()
+
+    failed=df.filter((col(column).isNotNull())& (col(column).cast('double').isNull())).count()
 
     return
     {   "field":column,
--- dq_checks/int_check.py
+++ dq_checks/int_check.py
@@ -3,7 +3,7 @@ from pyspark.sql.types import *
 
 def int_check(df,column):
     total=df.count()
-    failed=df.filter(typeof(column).cast(StringType())!="double").count()
+    failed=df.filter((col(column).isNotNull())& (col(column).cast('int').isNull())).count()
 
     return
     {   "field":column,
