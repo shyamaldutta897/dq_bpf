c83bf1a0e435c41a9f0bf22eacb12316acbd6b95
Shyamal Dutta <shyamaldutta897@gmail.com>
2026-05-03 11:11:23 +0530

    updated check functions

--- dq_checks/int_check.py
+++ dq_checks/int_check.py
@@ -2,7 +2,7 @@ from pyspark.sql.functions import *
 
 def int_check(df,column):
     total=df.count()
-    failed=df.filter(typeof(column).cast(str)!="double").count()
+    failed=df.filter(typeof(column).cast(string)!="double").count()
 
     return
     {   "field":column,
